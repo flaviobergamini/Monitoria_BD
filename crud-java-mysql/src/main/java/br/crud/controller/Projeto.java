7b844268738f2a42928d172f0afff46ca4100876
flaviobergamini <flaviohenrique@gec.inatel.br>
2021-09-16 14:07:09 -0300

    Removendo variáveis de autenticação de banco na classe principal, elas não tinham uso no local

--- crud-java-mysql/src/main/java/br/crud/controller/Projeto.java
+++ crud-java-mysql/src/main/java/br/crud/controller/Projeto.java
@@ -3,12 +3,7 @@ package br.crud.controller;
 import java.sql.*;
 
 public class Projeto {
-
-    static final String user = "root";
-    static final String password = "/MS-DOSV.6.22b";
-    static final String database = "exercicio";
-    static final String url = "jdbc:mysql://localhost:3306/" + database + "?useTimezone=true&serverTimezone=UTC&useSSL=false";
-
+    
     public static void main(String[] args) {
         Database database = new Database();
         database.connect();
